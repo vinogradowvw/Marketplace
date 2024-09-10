a4f492b5a41e2f8b1a80598e7177e8036e8af657
Daniil Sandakov <sandadan@fit.cvut.cz>
2024-09-11 00:41:28 +0300

    Added new beans for authorization.

--- src/main/java/com/marketplace/demo/config/SecurityConfig.java
+++ src/main/java/com/marketplace/demo/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -13,10 +15,12 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -41,7 +45,7 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults())
                 .logout(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .addFilterAfter(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
@@ -61,5 +65,30 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public RoleHierarchy roleHierarchy(){
+
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+
+        String hierarchy = "ROLE_ADMIN > ROLE_MODERATOR \n ROLE_MODERATOR > ROLE_DEVELOPER \n ROLE_SELLER > ROLE_USER \n" +
+                "ROLE_DEVELOPER > ROLE_USER \n ROLE_SUPPORT_AGENT > ROLE_USER \n ROLE_CONTENT_MANAGER > ROLE_USER \n" +
+                "ROLE_MODERATOR > ROLE_SELLER \n ROLE_MODERATOR > ROLE_SUPPORT_AGENT \n ROLE_MODERATOR > ROLE_CONTENT_MANAGER";
+
+        roleHierarchy.setHierarchy(hierarchy);
+        return roleHierarchy;
+    }
+
+    @Bean
+    public DefaultWebSecurityExpressionHandler webSecurityExpressionHandler() {
+        DefaultWebSecurityExpressionHandler expressionHandler = new DefaultWebSecurityExpressionHandler();
+        expressionHandler.setRoleHierarchy(roleHierarchy());
+        return expressionHandler;
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(DefaultWebSecurityExpressionHandler webSecurityExpressionHandler){
+        return (web -> web.expressionHandler(webSecurityExpressionHandler));
     }
 }
--- src/main/java/com/marketplace/demo/service/UserService/UserService.java
+++ src/main/java/com/marketplace/demo/service/UserService/UserService.java
@@ -7,7 +7,6 @@ import com.marketplace.demo.service.CrudServiceImpl;
 import com.marketplace.demo.service.ImageService.ImageService;
 import com.marketplace.demo.service.JWTService;
 import com.marketplace.demo.service.SubscriptionService.SubscriptionService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.repository.CrudRepository;
