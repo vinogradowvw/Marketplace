2a0c53af42fa49c1418bcc308d996c8fb66ed6bc
Vladimir Fedoseev <fedosvla@fit.cvut.cz>
2024-09-23 23:16:42 +0200

    Database drop ban

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,9 @@
-DROP SCHEMA public CASCADE;
-CREATE SCHEMA public;
-
-DROP SEQUENCE IF EXISTS users_seq;
-DROP SEQUENCE IF EXISTS cart_seq;
-DROP SEQUENCE IF EXISTS role_seq;
+-- DROP SCHEMA public CASCADE;
+-- CREATE SCHEMA public;
+--
+-- DROP SEQUENCE IF EXISTS users_seq;
+-- DROP SEQUENCE IF EXISTS cart_seq;
+-- DROP SEQUENCE IF EXISTS role_seq;
 
 CREATE SEQUENCE IF NOT EXISTS cart_seq start with 1 increment by 50;
 CREATE SEQUENCE IF NOT EXISTS image_seq start with 1 increment by 50;
